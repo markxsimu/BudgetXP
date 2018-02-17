357a7b4cdfa1ab69bcb25b5d81c182717150d2cc
Carloslll <carlos.walkerlll@gmail.com>
2018-02-17 17:55:02 -0500

    Food Activity/xml for food button

---
+++ app/src/main/java/com/example/markxsimu/budgetxp/FoodButtonActivity.java
@@ -0,0 +1,13 @@
+package com.example.markxsimu.budgetxp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_button);
+    }
+}
